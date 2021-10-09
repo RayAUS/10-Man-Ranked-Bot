1a9055b26999605d30dbb39b0062bcbbbd1b4dc8
RayAUS <88884424+RayAUS@users.noreply.github.com>
2021-10-09 16:22:00 +0000

    Added README.md and created basic bot

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+//Imports
 const { Client, Intents } = require('discord.js');
 
+//Bot's Permissions
 const client = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.DIRECT_MESSAGE_REACTIONS
   ]
 })
 
+//Bots Initial Call
+client.
+
+
+//Bot Login
 client.login(process.env.DISCORD_BOT_TOKEN)
